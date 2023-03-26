8a7b1aae72c4b027a92fba07362589952ff60c54
thallerdavid <david.thaller123@gmail.com>
2023-03-26 22:41:40 +0200

    post request

--- src/main/java/htl/leonding/GreetingResource.java
+++ src/main/java/htl/leonding/GreetingResource.java
@@ -4,10 +4,7 @@ package htl.leonding;
 import htl.leonding.outgoing.MqttPublischer;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/resource")
@@ -21,14 +18,15 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String send(@PathParam("value") String value) {
         mqttPublischer.send(value);
-        return "Message sent!";
+        return "Message sent! " + value;
     }
 
-    @GET
-    @Path("/sendToRasa/{value}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String sendToRasa(@PathParam("value") String value) {
-        mqttPublischer.sendToRasa(value);
-        return "Message sent to Rasa!";
+    @POST
+    @Path("/send/{value}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String sendPost(@PathParam("value") String value) {
+        return value;
     }
+
 }
--- src/main/java/htl/leonding/outgoing/MqttPublischer.java
+++ src/main/java/htl/leonding/outgoing/MqttPublischer.java
@@ -5,24 +5,33 @@ import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Set;
 
 public class MqttPublischer {
 
     @Inject @Channel("outgoing-channel")
     Emitter<String> emitter;
 
+
     //payload = Nutzlast
     public void send(String payload) {
-        String topic = "itp/leonie/thaller";
+        String topic = "itp/leonie/message";
 
         MqttMessage message = MqttMessage.of(topic, payload);
         emitter.send(message);
+        sendToRasa(payload);
     }
 
-    public void sendToRasa(String payload) {
-        String topic = "itp/rasa/thaller";
+    public String sendToRasa(String payload) {
+        String topic = "itp/rasa/message";
 
         MqttMessage message = MqttMessage.of(topic, payload);
         emitter.send(message);
+        return String.valueOf(message);
     }
 }
